80c65cad01d06271e2d870d2552bb5317f9d54e9
Josh Summitt <josh@otto-js.com>
2022-07-01 16:22:16 -0500

    Get CSP from S3 and support testing revisions

---
+++ fastly/README.md
@@ -0,0 +1,8 @@
+### Steps to get running
+- Install fastly CLI tool `https://github.com/fastly/cli/releases`
+- Initialize fastly `fastly profile create`
+- Enter Your Api token. You need to log into fastly and create a personal api token in your user settings. 
+
+### To run locally
+-   `fastly compute serve`
+-
--- fastly/src/index.js
+++ fastly/src/index.js
@@ -4,36 +4,73 @@ import { decode } from "base-64"
 const handler = async (event) => {
   // ENTER OTTO API KEY
   const dict = new Dictionary("otto")
-  const otto_key=dict.get("key") //,"0066c2c5-3306-4529-879e-1fcb4aed675c")
-  const otto_host=dict.get("apihost") //, "dev-api.devconops.com")
-  const site_host=dict.get("host") //, "delicate-dew-0422.on.fleek.co")
+  const ottoKey=dict.get("key") 
+  const ottoHost=dict.get("apihost") 
+  const siteHost=dict.get("host") 
 
 
-  const otto_url=`https://${otto_host}/api/v2/script/${otto_key}.js`
+
   // Get the request from the client.
   const req = event.request
-    req.headers.set("Host", site_host)
-    let apiReq = new Request(otto_url);
+  const ottoCSPUrl=getCSPUrl(req.url, ottoHost, ottoKey)
+  req.headers.set("Host", siteHost)
+  const apiReq = new Request(ottoCSPUrl);
+  
 
-    let apiRes = await fetch(apiReq, { backend: "origin_1"} );
-    let apiResBody = await apiRes.text();
-    let start = apiResBody.indexOf('"policy":"')
-    let csp = apiResBody.substring(start+10, apiResBody.indexOf('"', start+11))
-    if(otto_host.indexOf("dev-api") != -1){
-      csp = csp.replace("report-uri https://analyticssystems.net/csp/report/", `report-uri https://dev-api.devconops.com/api/v2/csp/report/`)
-    }else{
-      csp = csp.replace("report-uri https://analyticssystems.net/csp/report/", `report-uri https://analyticssystems.net/api/v2/csp/report/`)
-    }
+  const apiRes = await fetch(apiReq, { backend: "origin_1"} );
 
-    const beresp = await fetch(req, {
-      backend: "origin_0"
-    })
 
-    beresp.headers.set("content-security-policy", csp);
+  const beresp = await fetch(req, {
+    backend: "origin_0"
+  })
 
-    // Send our response back to the client.
-    //return beresp;
-    return beresp;
+  if(apiRes.status == "200"){
+    const csp = await apiRes.json()
+    beresp.headers.set("content-security-policy", csp.policy);
+  }
+
+
+  // Send our response back to the client.
+  //return beresp;
+  return beresp;
 };
 
 addEventListener("fetch", event => event.respondWith(handler(event)));
+
+function getCSPUrl(url, ottoHost, ottoKey){
+  const revUrl = getRevsionIdURL(url, ottoHost, ottoKey)
+  if(revUrl == null){
+    return `https://${ottoHost}/csp/${ottoKey}.json`
+  }else{
+    return revUrl
+  }
+}
+
+function getRevsionIdURL(url, ottoHost, ottoKey){
+  const revId = getRevisionId(url)
+  if(revId){
+    return `https://${ottoHost}/csp/${ottoKey}-${revId}.json`
+  }else{
+    return null
+  }
+}
+
+function getRevisionId(url){
+  try{
+    if(url.indexOf("?") != -1 && url.indexOf("otto_revision_id") != -1){
+      const qs = url.split("?")
+      if(qs.length > 1){
+        const params = qs[1].split("&")
+        const revision = params.filter( p => p.indexOf("otto_revision_id=") != -1)
+        if(revision){
+          const keyValue = revision[0].split("=")
+          return keyValue[1]
+        }
+      }
+    }
+  }catch(e){
+    console.log(e)
+  }
+  return null
+}
+
